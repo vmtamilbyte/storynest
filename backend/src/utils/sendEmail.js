49fb81a52cbc9f4b13fb432a12ee311352013109
vmtamilbyte <tamilvenkatesan2k@gmail.com>
2026-08-15 21:23:55 +0530

    fix: switch from SMTP to Resend API for reliable email delivery on Render

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,16 +1,10 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"StoryNest" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: 'StoryNest <onboarding@resend.dev>',
     to,
     subject,
     html,
